33d53269e571228159759f9b4e1588b61e3b8717
Zulfian <iiizulfiannn@gmail.com>
2020-07-30 20:27:01 +0700

    update api firebase

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const Firebase = firebase.initializeApp({
-  apiKey: 'AIzaSyBpc_CCRkA4pFXPWoZgEiIkeet7D2xfi1c',
-  authDomain: 'hayapp-bbec2.firebaseapp.com',
-  databaseURL: 'https://hayapp-bbec2.firebaseio.com',
-  projectId: 'hayapp-bbec2',
-  storageBucket: 'hayapp-bbec2.appspot.com',
-  messagingSenderId: '416535283252',
-  appId: '1:416535283252:web:e9da98f286a145b82fec61',
+  apiKey: 'AIzaSyAgaRtN4FVK144ePkYI4_HAWY99ivjek8A',
+  authDomain: 'hayapp-284909.firebaseapp.com',
+  databaseURL: 'https://hayapp-284909.firebaseio.com',
+  projectId: 'hayapp-284909',
+  storageBucket: 'hayapp-284909.appspot.com',
+  messagingSenderId: '747521009047',
+  appId: '1:747521009047:web:fb1ef9152ba0606a3a90d2',
 });
 
 export default Firebase;
